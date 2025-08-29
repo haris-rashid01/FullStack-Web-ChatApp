f881bc0219bdf2832ee812b4de0572d726a02e31
Haris Rashid <haris.rashidch@gmail.com>
2025-08-30 01:19:11 +0500

    Updated UI and components

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 
-if (process.env.ENV == "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
