5fc94efba0a1e42dddd2edc3b02c9c14286910d3
Haris Rashid <haris.rashidch@gmail.com>
2025-08-30 01:26:26 +0500

    Updated UI and components

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000": "/"
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -88,17 +89,17 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
-      query:{
-        userId: authUser._id
-      }
+      query: {
+        userId: authUser._id,
+      },
     });
 
     socket.connect();
     set({ socket: socket });
 
-    socket.on("getOnlineUsers", (userIds)=>{
-      set({onlineUsers: userIds})
-    })
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
   },
   disconnectSocket: () => {
     if (get().socket?.connected) get().socket.disconnect();
