7b55b5deac5fdbf427d20f1559e7e0d626b74778
Haris Rashid <haris.rashidch@gmail.com>
2025-10-19 22:51:54 +0500

    updated app

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,34 +8,32 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import path from "path";
-dotenv.config();
 
-const port = process.env.PORT;
+dotenv.config();
+const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   })
 );
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/groups", groupRoutes);
 
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
+
 server.listen(port, () => {
   console.log("Server is listening at " + port);
   connectDB();
