4ef8ad445b5a64ef26d56b0d4b849ea0ad64750e
Haris Rashid <haris.rashidch@gmail.com>
2025-10-19 22:04:10 +0500

    updated app

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,16 +23,13 @@ app.use(
   })
 );
 
-
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/groups", groupRoutes);
