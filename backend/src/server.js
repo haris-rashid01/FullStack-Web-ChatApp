fadbc820dc861f806fa764972a991eda56a3b25b
Haris Rashid <haris.rashidch@gmail.com>
2025-10-19 21:37:46 +0500

    updated app

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,8 +13,8 @@ dotenv.config();
 const port = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -23,12 +23,16 @@ app.use(
   })
 );
 
+
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../dist", "index.html"));
   });
 }
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/groups", groupRoutes);
